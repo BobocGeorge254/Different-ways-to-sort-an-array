0340bf92783961ffb9d6efa036370dc56d0d700c
George Boboc <88781174+BobocGeorge254@users.noreply.github.com>
2022-03-02 09:49:11 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,24 +2,31 @@
 #include <time.h>
 #include <stdlib.h>
 #include <algorithm>
+#include <vector>
 #define NMAX 1000005
 
 using namespace std;
 
+vector <int> MS, QS, RS, STLS , SS, BS ;
 
-int MS[NMAX], QS[NMAX], SS[NMAX], RS[NMAX], BS[NMAX], STLS[NMAX];
 void Test (int n){
-    for (int i = 1 ; i <= n ; ++i)
-        MS[i] = QS[i] = SS[i] = RS[i] = BS[i] = STLS[i] = rand()% INT32_MAX;
+    for (int i = 1 ; i <= n ; ++i) {
+        MS.push_back(rand()% INT_MAX ) ;
+        QS.push_back(rand()% INT_MAX ) ;
+        RS.push_back(rand()% INT_MAX ) ;
+        STLS.push_back(rand()% INT_MAX ) ;
+        SS.push_back(rand()% INT_MAX ) ;
+        BS.push_back(rand()% INT_MAX ) ;
+    }
 }
 
-void Merge(int a[], int left, int mid, int right){
-    int b[NMAX];
+void Merge (vector <int>& a, int left, int mid, int right){
+    vector <int> b;
     int x = left , k = 1 , y = mid + 1 ;
     while ( x <= mid && y <= right )
         if (a[x] < a[y])
             b[k++] = a[x++];
-        else 
+        else
             b[k++] = a[y++];
     while (x <= mid)
         b[k++] = a[x++];
@@ -31,7 +38,7 @@ void Merge(int a[], int left, int mid, int right){
 
 }
 
-void MergeSort(int a[], int left, int right) {
+void MergeSort(vector <int>& a, int left, int right) {
     if ( left < right ) {
         int mid = (left + right) / 2 ;
         MergeSort(a,left,mid);
@@ -48,23 +55,23 @@ int get_Max(int a[], int n){
     return cand;
 }
 
-void Radix_Count(int a[], int n, int index) {
+void Radix_p_count(int a[], int n, int index) {
     const int max = 10;
     int output[n];
-    int count[max];
+    int p_count[max];
 
     for (int i = 0 ; i < max ; ++i )
-        count[i] = 0 ;
-    
+        p_count[i] = 0 ;
+
     for (int i = 0 ; i < n ; ++i )
-        count[(a[i] / index) % 10]++ ;
-    
+        p_count[(a[i] / index) % 10]++ ;
+
     for (int i = 1 ; i < max ; ++i )
-        count[i] += count[i - 1];
+        p_count[i] += p_count[i - 1];
 
     for (int i = n - 1; i >= 0 ; --i ) {
-        output[count[(a[i] / index) % 10] - 1] = a[i];
-        count[(a[i] / index) % 10] -- ;
+        output[p_count[(a[i] / index) % 10] - 1] = a[i];
+        p_count[(a[i] / index) % 10] -- ;
     }
 
     for ( int i = 0 ; i < n ; ++i )
@@ -74,16 +81,10 @@ void Radix_Count(int a[], int n, int index) {
 void RadixSort(int a[], int n) {
     int maxv = get_Max(a,n);
     for ( int index = 1 ; maxv / index > 0 ; index *= 10)
-        Radix_Count(a,n,index);
+        Radix_p_count(a,n,index);
 }
 
-void swap(int a, int b){
-    int t = a;
-    a = b;
-    b = t;
-}
-
-int Partition(int a[], int left, int right) {
+int Partition(vector <int>& a, int left, int right) {
     int pivot = a[right];
     int i = (left - 1) ;
     for ( int j = left ; j < right ; j ++ ) {
@@ -96,7 +97,7 @@ int Partition(int a[], int left, int right) {
     return (i + 1);
 }
 
-void QuickSort(int a[], int left, int right) {
+void QuickSort(vector <int>& a, int left, int right) {
     if (left < right) {
         int pi = Partition(a,left,right);
             if (pi - left < right - pi) {
@@ -110,7 +111,7 @@ void QuickSort(int a[], int left, int right) {
     }
 }
 
-void ShellSort(int a[],int n){
+void ShellSort(vector <int>& a,int n){
     for ( int gap = n / 2 ; gap > 0 ; gap /= 2) {
         for ( int i = gap ; i < n ; i ++ ) {
             int temp = a[i] ;
@@ -122,11 +123,11 @@ void ShellSort(int a[],int n){
     }
 }
 
-void BubbleSort(int a[], int n){
+void BubbleSort(vector <int>& a, int n){
     bool Ok = true;
     while (Ok == true) {
         Ok = false;
-        for (int i = 1 ; i < n ; ++i)
+        for (int i = 0 ; i < n - 1 ; ++i)
             if (a[i] > a[i + 1]) {
                 swap(a[i], a[i + 1]);
                 Ok = true ;
@@ -137,38 +138,40 @@ void BubbleSort(int a[], int n){
 int main(){
 
     const int N = 6;
-    int length[] = {50,100,1000,10000,100000,1000000};
-    int count;
-    for (int i = 1 ; i <= 6 ; ++i ){
+    srand(time(NULL));
+    int p_count;
+    int start,stop,length[]= {50,100,1000,10000,100000,1000000};
+    for (int i = 1 ; i <= 5 ; ++i ){
         cout << "Testul" << " " << i << " : " << "( " << length[i - 1] << " elemente )" << endl ;
-        count = length[i - 1];
-        Test(count);
-        int start, stop, startM, stopM;
+        p_count = length[i - 1];
+        Test(p_count);
+        /*
         start = clock();
-        MergeSort(MS,1,count);
+        MergeSort(MS,1,p_count);
         stop = clock();
         cout << "MergeSort:" << fixed << double(stop-start)/CLOCKS_PER_SEC << endl ;
+        */
         start = clock();
-        QuickSort(QS,1,count);
+        QuickSort(QS,1,p_count);
         stop = clock();
         cout << "QuickSort:" << fixed << double(stop-start)/CLOCKS_PER_SEC << endl ;
+        /*
         start = clock();
-        RadixSort(RS,count);
+        RadixSort(RS,p_count);
         stop = clock();
         cout << "RadixSort:" << fixed << double(stop-start)/CLOCKS_PER_SEC << endl ;
+        */
         start = clock();
-        ShellSort(SS,count);
+        ShellSort(SS,p_count);
         stop = clock();
         cout << "ShellSort:" << fixed << double(stop-start)/CLOCKS_PER_SEC << endl ;
         start = clock();
-        ShellSort(BS,count);
+        BubbleSort(BS,p_count);
         stop = clock();
         cout << "BubbleSort:" << fixed << double(stop-start)/CLOCKS_PER_SEC << endl ;
         start = clock();
-        sort(STLS + 1, STLS + count + 1);
+        sort(STLS.begin(),STLS.end());
         stop = clock();
         cout << "STLSort:" << fixed << double(stop-start)/CLOCKS_PER_SEC << endl << endl << endl ;
     }
-
-
 }
