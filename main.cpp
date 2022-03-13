ddac184694be4f15a5ffb7a2a7c017dc4e68a47a
George Boboc <88781174+BobocGeorge254@users.noreply.github.com>
2022-03-13 02:01:48 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,14 +9,14 @@ typedef unsigned long long ULL;
 
 using namespace std;
 
-vector <ULL> MS, QS, RS, STLS , SS, IS ;
+vector <ULL> MS, QS, QSR, RS10, RS65536, STLS , SS, IS ;
 
 
 //deoarece functia rand() returneaza valori pe 2 octeti, va trebui sa generam un numar pe 8 octeti prin a genera
 //fiecare bit din configuratia sa.
 ULL Random() {
     ULL x = 0 ;
-    for (int i = 0 ; i < 64 ; ++i ) {
+    for (int i = 0 ; i < 63 ; ++i ) {
         bool bit = rand() % 2 ;
         x = x + bit * ( 1 << i ) ;
     }
@@ -28,10 +28,12 @@ ULL Random() {
 //folosesc 6 vectori diferiti, cu nume sugestive, pentru a retine numerele de sortat.
 void Test (int n){
     for (int i = 1 ; i <= n ; ++i) {
-        ULL x = Random()%1000;
+        ULL x = Random();
         MS.push_back(x) ;
         QS.push_back(x) ;
-        RS.push_back(x) ;
+        QSR.push_back(x);
+        RS10.push_back(x) ;
+        RS65536.push_back(x);
         STLS.push_back(x) ;
         SS.push_back(x) ;
         IS.push_back(x) ;
@@ -73,17 +75,17 @@ void MergeSort(vector <ULL>& a, int left, int right) {
 
 ULL get_max(vector<ULL> v){
     ULL mx = v.front();
-    for (auto i: v)
-        if (i > mx)
-            mx = i;
+    for ( int i = 0 ; i < v.size(); ++i )
+        if (v[i] > mx)
+            mx = v[i];
     return mx;
 }
 
 void count_sort_by_exp(vector<ULL>& v, ULL exp, int base){
     vector<ULL> output(v.size()), cnt(base, 0);
 
-    for(auto i: v)
-        cnt[(i/exp) % base]++;
+    for(int i = 0 ; i < v.size(); ++i)
+        cnt[(v[i]/exp) % base]++;
 
     for(int i = 1; i < base; i++)
         cnt[i] += cnt[i-1];
@@ -97,8 +99,8 @@ void count_sort_by_exp(vector<ULL>& v, ULL exp, int base){
 void count_sort_by_exp_65536(vector<ULL>& v, ULL exp, int base){
     vector<ULL> output(v.size()), cnt(base, 0);
 
-    for(auto i: v)
-        cnt[(i/exp) >> 16]++;
+    for(int i = 0 ; i < v.size(); ++i )
+        cnt[(v[i]/exp) >> 16]++;
 
     for(int i = 1; i < base; i++)
         cnt[i] += cnt[i-1];
@@ -138,6 +140,22 @@ void QuickSort(vector <ULL>& a, int left, int right) {
         ULL pi = Partition(a,left,right);
         QuickSort(a,left,pi - 1);
         QuickSort(a,pi + 1, right);
+    }
+}
+
+ULL PartitionRandom(vector <ULL>& a, int left, int right) {
+    int index = left ;
+    ULL x = Random()%(right - left)  ;
+    index = index + x ;
+    swap(a[index], a[right]);
+    return Partition(a,left,right);
+}
+
+void QuickSortRandom(vector <ULL>& a, int left, int right) {
+    if (left < right) {
+        ULL pi = PartitionRandom(a,left,right);
+        QuickSortRandom(a,left,pi - 1);
+        QuickSortRandom(a,pi + 1, right);
     }
 }
 
@@ -191,11 +209,10 @@ int main(){
     srand(time(NULL));
     int test_length;
     int start,stop,length[]= {50,100,1000,10000,100000,1000000, 10000000};
-    for (int i = 1 ; i <= 2 ; ++i ){
+    for (int i = 1 ; i <= 7 ; ++i ){
         cout << "Testul" << " " << i << " - " << " " << length[i - 1] << " elemente " << endl ;
         test_length = length[i - 1];
         Test(test_length);
-        //vprint(IS);
 
 
         start = clock();
@@ -207,30 +224,48 @@ int main(){
         start = clock();
         QuickSort(QS,0,test_length - 1);
         stop = clock();
-        cout << "QuickSort: " << fixed << double(stop-start)/CLOCKS_PER_SEC << " secunde" << endl ;
+        cout << "QuickSort: - pivot nerandomizat: " << fixed << double(stop-start)/CLOCKS_PER_SEC << " secunde" << endl ;
         //vprint(QS);
+
+        start = clock();
+        QuickSortRandom(QSR,0,test_length - 1);
+        stop = clock();
+        cout << "QuickSort: - pivot randomizat: " << fixed << double(stop-start)/CLOCKS_PER_SEC << " secunde" << endl ;
+        //vprint(QSR);
 
         /*
         start = clock();
-        RadixSort(RS,10);
+        RadixSort(RS10,10);
         stop = clock();
-        cout << "RadixSort: " << fixed << double(stop-start)/CLOCKS_PER_SEC << " secunde" << endl ;
-        vprint(RS); */
+        cout << "RadixSort - baza 10: " << fixed << double(stop-start)/CLOCKS_PER_SEC << " secunde" << endl ;
+        //vprint(RS10);
+        */
+
+        /*
+        start = clock();
+        RadixSort(RS65536,65536);
+        stop = clock();
+        cout << "RadixSort - baza 2^16: " << fixed << double(stop-start)/CLOCKS_PER_SEC << " secunde" << endl ;
+        vprint(RS65536);
+        */
+
 
         start = clock();
-        vprint(SS);
         ShellSort(SS,test_length - 1);
-        vprint(SS);
         stop = clock();
         cout << "ShellSort: " << fixed << double(stop-start)/CLOCKS_PER_SEC << " secunde" << endl ;
         //vprint(SS);
 
 
+        if ( i <= 5 ) {
         start = clock();
         InsertionSort(IS,test_length - 1);
         stop = clock();
         cout << "InsertionSort: " << fixed << double(stop-start)/CLOCKS_PER_SEC << " secunde"<< endl ;
         //vprint(IS);
+        }
+        else
+            cout << "InsertionSort: " << "failed"<< endl ;
 
         start = clock();
         sort(STLS.begin(),STLS.end());
